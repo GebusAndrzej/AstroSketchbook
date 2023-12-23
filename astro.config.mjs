0b7366955669b72a3987c1d22b1040c7da0ef766
Andrzej Gebus <andrzejgebuss@gmail.com>
2023-12-23 19:26:54 +0100

    gh actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gebusandrzej.github.io/',
+  base: '/AstroSketchbook',
   integrations: [react(), tailwind()]
 });
